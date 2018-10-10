85d3db8a83de12694fb00afd2f7ae39dfe069465
Matthieu Court <matthieu.court@gmail.com>
2018-10-10 02:57:55 -0400

    sloppy addition of tweepy donald Trump search with some baby sentiment analysis in the mix

--- app.py
+++ app.py
@@ -2,11 +2,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import requests
+import tweepy
+from textblob import TextBlob
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# some python testing stuff
+consumer_key = 'ueAdq7eQ5fOj3K1Ev8vdjh9KL'
+consumer_secret = 'PKyfaVVPVzNlYtltqC5tDzntzq2PR22Tga1F8S9RCBVerKHSlS'
 
+access_token = '166724523-s1l3OC589hHI3k4rLpsy12dRNK7j8F4g5c7CJdlt'
+access_token_secret = 'YkqC9CrsEbRyz2ExggUHXu9WJckzsxDnvAGSQJUltxTzX'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+public_tweets = api.search('Trump')
+
+# print the Donald tweets to console
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
